b83569eec8f9b614b6ec1c321926f98e0c31d511
Kristijan19 <kristijan.konda16@gmail.com>
2025-12-24 09:07:35 +0100

    Routes Reloaded

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,13 +3,63 @@
 namespace App\Http\Controllers;
 
 use App\Models\JobListing;
+use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
     public function index()
     {
-        $jobs = JobListing::with('employer')->paginate(3);
-
+        $jobs = JobListing::with('employer')->paginate(5);
         return view('jobs.index', compact('jobs'));
     }
+
+    public function show(JobListing $job)
+    {
+        return view('jobs.show', compact('job'));
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+            'salary' => 'required|numeric',
+        ]);
+
+        JobListing::create([
+            'title' => $request->title,
+            'salary' => $request->salary,
+            'employer_id' => null,
+        ]);
+
+        return redirect('/jobs');
+    }
+
+    public function edit(JobListing $job)
+    {
+        return view('jobs.edit', compact('job'));
+    }
+
+    public function update(Request $request, JobListing $job)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+            'salary' => 'required|numeric',
+        ]);
+
+        $job->update($request->only('title', 'salary'));
+
+        return redirect("/jobs/{$job->id}");
+    }
+
+    public function destroy(JobListing $job)
+    {
+        $job->delete();
+        return redirect('/jobs');
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,76 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\JobListing;
+use App\Http\Controllers\JobController;
 
 /*
 |--------------------------------------------------------------------------
 | Static pages
 |--------------------------------------------------------------------------
 */
-Route::get('/', function () {
-    return view('home', ['name' => 'Guest']);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/', 'home', ['name' => 'Guest']);
+Route::view('/contact', 'contact');
 
 /*
 |--------------------------------------------------------------------------
-| Jobs
+| Jobs (RESOURCE ROUTES)
 |--------------------------------------------------------------------------
 */
-Route::get('/jobs', function () {
-    $jobs = JobListing::with('employer')->paginate(5);
-    return view('jobs.index', compact('jobs'));
-});
+Route::resource('jobs', JobController::class);
 
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-Route::post('/jobs', function (Request $request) {
-    $request->validate([
-        'title' => 'required|min:3',
-        'salary' => 'required|numeric',
-    ]);
-
-    JobListing::create([
-        'title' => $request->title,
-        'salary' => $request->salary,
-        'employer_id' => null,
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = JobListing::with('employer')->findOrFail($id);
-    return view('jobs.show', compact('job'));
-});
-
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = JobListing::findOrFail($id);
-    return view('jobs.edit', compact('job'));
-});
-
-Route::patch('/jobs/{id}', function (Request $request, $id) {
-    $request->validate([
-        'title' => 'required|min:3',
-        'salary' => 'required|numeric',
-    ]);
-
-    $job = JobListing::findOrFail($id);
-    $job->update($request->only('title', 'salary'));
-
-    return redirect("/jobs/$id");
-});
-
-Route::delete('/jobs/{id}', function ($id) {
-    JobListing::findOrFail($id)->delete();
-    return redirect('/jobs');
-});
 
 
